42ece3c86f97af1787f0c1980290c72811e7383c
Fia <94110257+fiababiakandersson@users.noreply.github.com>
2025-08-06 12:38:00 +0200

    clean up fe + change title and favicon

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,11 +1,14 @@
 import { ApolloProvider } from "@apollo/client";
+import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { client } from "./lib/apolloClient.ts";
 
 createRoot(document.getElementById("root")!).render(
-  <ApolloProvider client={client}>
-    <App />
-  </ApolloProvider>
+  <React.StrictMode>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </React.StrictMode>
 );
