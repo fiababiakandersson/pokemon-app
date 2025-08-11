91bd8bab02f4ce8d8ba6eab62041a3f9278102b7
Fia <94110257+fiababiakandersson@users.noreply.github.com>
2025-08-11 15:57:47 +0200

    styling and logo

--- frontend/src/components/NavigationHeader.tsx
+++ frontend/src/components/NavigationHeader.tsx
@@ -1,4 +1,5 @@
 import { useQuery } from "@apollo/client";
+import FilterAltOutlinedIcon from "@mui/icons-material/FilterAltOutlined";
 import FormControl from "@mui/material/FormControl";
 import InputLabel from "@mui/material/InputLabel";
 import MenuItem from "@mui/material/MenuItem";
@@ -22,53 +23,103 @@ export default function NavigationHeader(props: Props) {
   };
 
   return (
-    <div style={{ backgroundColor: "#dc4151", height: 200 }}>
-      <h4>Filter</h4>
-      <FormControl
-        sx={{
-          m: 1,
-          minWidth: 120,
-          "& .MuiOutlinedInput-root": {
-            "& fieldset": {
-              border: "lightGrey 1px solid",
-            },
-            "&.Mui-focused fieldset": {
-              border: "lightGrey 1px solid",
-            },
-            "&:hover fieldset": {
-              border: "lightGrey 1px solid",
-            },
-          },
+    <div
+      style={{
+        backgroundColor: "#dc4151",
+        position: "relative",
+        height: 200,
+      }}
+    >
+      <img
+        src="../public/logo.png"
+        alt="Pokemon logo"
+        style={{
+          width: 250,
+          position: "absolute",
+          top: "40%",
+          left: "50%",
+          transform: "translate(-50%, -50%)",
         }}
-        size="small"
+      />
+      <div
+        style={{
+          position: "absolute",
+          maxWidth: "50rem",
+          bottom: 20,
+          left: 30,
+        }}
       >
-        <InputLabel
-          id="demo-select-small-label"
-          sx={{
-            "&.Mui-focused": {
-              color: "grey",
-            },
+        <div
+          style={{
+            margin: "0 0 5px 10px",
+            alignItems: "center",
+            display: "flex",
+            color: "white",
+            fontSize: 15,
+            gap: 5,
           }}
         >
-          Type
-        </InputLabel>
+          <p> Filter</p>
 
-        <Select
-          labelId="demo-select-small-label"
-          id="demo-select-small"
-          value={props.typeFilter}
-          onChange={handleChange}
+          <FilterAltOutlinedIcon
+            sx={{
+              fontSize: 21,
+            }}
+          />
+        </div>
+        <FormControl
+          sx={{
+            m: 1,
+            width: 140,
+            "& .MuiOutlinedInput-root": {
+              "& fieldset": {
+                border: "white 1px solid",
+              },
+              "&.Mui-focused fieldset": {
+                border: "white 1px solid",
+              },
+              "&:hover fieldset": {
+                border: "white 1px solid",
+              },
+            },
+          }}
+          size="small"
         >
-          <MenuItem value="">
-            <em>All types</em>
-          </MenuItem>
-          {data.types?.map((type: string, i: number) => (
-            <MenuItem key={i} value={type}>
-              {type.charAt(0).toUpperCase() + type.slice(1)}
+          <InputLabel
+            id="demo-select-small-label"
+            sx={{
+              color: "white",
+              "&.Mui-focused": {
+                color: "white",
+              },
+            }}
+          >
+            Type
+          </InputLabel>
+
+          <Select
+            labelId="demo-select-small-label"
+            id="demo-select-small"
+            value={props.typeFilter}
+            onChange={handleChange}
+            sx={{
+              color: "white",
+              "& .MuiSvgIcon-root": {
+                color: "white",
+              },
+            }}
+          >
+            <MenuItem value="">
+              <em>All types</em>
             </MenuItem>
-          ))}
-        </Select>
-      </FormControl>
+            {data.types?.map((type: string, i: number) => (
+              <MenuItem key={i} value={type}>
+                {type.charAt(0).toUpperCase() + type.slice(1)}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </div>
     </div>
   );
 }
